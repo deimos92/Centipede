152cfb4d1b28e14434f6af14576789f71c899222
MChief1337 <stanislavperekrest92@gmail.com>
2019-05-03 18:18:24 +0300

    Initial commit

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    //Destroying any Other object crossing the boundary
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
